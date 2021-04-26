f94f07f1fa5f0f7f08d503e98a088857b8d15aab
doyeon <kkamang03@gmail.com>
2021-04-26 16:56:44 +0900

    Initial Upload

---
+++ AiFwTest/src/main/java/com/js/LoginController.java
@@ -0,0 +1,37 @@
+package com.js;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	@RequestMapping("/test")
+	public String test1() {
+		return "test1.html";
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doHandle(request, response);
+	}
+	
+	protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		String id = request.getParameter("id");
+		System.out.println("Data : " + id + "login ok\nStatus : success");
+	}
+}
---
+++ AiFwTest/src/main/resources/static/js/test.js
@@ -0,0 +1,14 @@
+/**
+ * test.js
+ */
+
+$(document).ready(function(){
+	$('#login_btn').click(function(){
+		var id = $('#id').val();
+		var pw = $('#pw').val();
+		
+		alert(id + ":" + pw);
+		
+		return false;
+	})
+});
